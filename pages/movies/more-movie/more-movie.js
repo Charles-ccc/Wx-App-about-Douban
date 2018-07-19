c934dd454365086317d5c20dc572becee2a88fae
Charles-ccc <liu930505@qq.com>
2018-07-19 18:14:52 +0800

    halo

--- pages/movies/more-movie/more-movie.js
+++ pages/movies/more-movie/more-movie.js
@@ -1,11 +1,14 @@
+// scroll-view不再支持下拉刷新 onPullDownRefresh()
+// 修改教程 https://www.zhihu.com/search?type=content&q=scroll-view
 let util = require('../../../utils/util.js');
 let app = getApp(); // 引用全局变量
 Page({
     data: {
-        movies: "",
+        movies: [],
         navigateTitle: "",
         requestUrl: "",
-        totalCount: 0
+        totalCount: 0,
+        isEmpty: true
     },
     onLoad(options) {
         var category = options.category;
@@ -30,11 +33,28 @@ Page({
         // 获取数据
         util.http(dataUrl, this.processDoubanData)
     },
-    onScrollLower(event) {
+    // 框架触发 scroll-view
+    // onScrollLower(event) {
+    //     var nextUrl = this.data.requestUrl + "?start=" + this.data.totalCount + "&count=20";
+    //     // 获取数据 改变了参数
+    //     util.http(nextUrl, this.processDoubanData);
+    //     wx.showNavigationBarLoading(); // 加载等待图
+    // },
+    onReachBottom: function(event) {
         var nextUrl = this.data.requestUrl + "?start=" + this.data.totalCount + "&count=20";
-        // 获取数据 改变了参数
         util.http(nextUrl, this.processDoubanData)
+        wx.showNavigationBarLoading()
     },
+    // 新增下拉刷新，也会触发下面方法
+    onPullDownRefreash(event) {
+        var refreshUrl = this.data.requestUrl + "?start=0&count=20";
+        // 将movies置空，将状态改为true，否则每次刷新都会加载相同的20条数据
+        this.data.movies = {};
+        this.data.Empty = true;
+        util.http(refreshUrl, this.processDoubanData);
+        wx.showNavigationBarLoading(); // 加载等待图
+    },
+
     processDoubanData(moviesDouban) {
         // moviesDouban 是 util中函数返回出来的 callBack(data)
         console.log(moviesDouban);
@@ -55,11 +75,24 @@ Page({
             }
             movies.push(temp)
         }
-        // 每在数据绑定之前，都给totalCount 加20
-        this.data.totalCount += 20;
+
+        var totalMovies = {};
+        //如果要绑定新加载的数据，那么需要同旧有的数据合并在一起
+        if (!this.data.Empty) {
+            // empty非空，不是第一次加载数据，所以要对数据进行叠加
+            totalMovies = this.data.movies.concat(movies);
+        } else {
+            totalMovies = movies;
+            // movies赋值给totalMovies之后，就不是第一次取数据了，所以false
+            this.data.Empty = false;
+        }
         this.setData({
-                movies: movies
+                movies: totalMovies
             })
+            // 每在数据绑定之后，都给totalCount 加20 ，数据累加
+        this.data.totalCount += 20;
+        wx.hideNavigationBarLoading() // 数据加载结束后，就隐藏
+        wx.stopPullDownRefresh() // 停止下拉刷新
             // console.log(movies);
     },
     onReady(event) {
